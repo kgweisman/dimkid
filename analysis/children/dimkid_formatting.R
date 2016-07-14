5f4714d6e9537669564d5ae5007475fa1c283e6d
kgweisman <kweisman@stanford.edu>
2016-07-14 08:43:16 -0700

    separate child and adult analyses

---
+++ analysis/children/dimkid_formatting.R
@@ -0,0 +1,75 @@
+# --- PRELIMINARIES -----------------------------------------------------------
+
+# libraries
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(lme4)
+library(jsonlite)
+library(stats)
+library(stringr)
+
+# clear environment
+rm(list=ls())
+
+# set up function for reading in data from JSON objects
+jsonFormat = function(wd, runName) {
+  # NOTE: requires dplyr and jsonlite packages
+  # library(jsonlite)
+  # library(dplyr)
+  
+  # set working directory
+  setwd(wd)
+  
+  # gather files
+  files = dir("individual sessions/")
+  
+  # make dataframe
+  d.raw = data.frame()
+  
+  for(i in 1:length(files)) {
+    
+    # read in file
+    d.temp <- read.csv(paste0("./individual sessions/", files[i])) %>%
+      mutate(run = runName)
+    
+    # bind into same dataframe
+    d.raw = bind_rows(d.raw, d.temp)
+    
+  }
+  
+  return(d.raw)
+}
+
+# --- READING IN DATA OBJECTS -------------------------------------------------
+
+# Kid run 01 (2016-06-28 to ...)
+d_run_01 = jsonFormat(
+  wd = "/Users/kweisman/Documents/Research (Stanford)/Projects/Dimkid/data/",
+  runName = "run_01")
+
+# --- TIDYING -----------------------------------------------------------------
+
+# clean up variables
+d_tidy = d_run_01 %>%
+  # full_join(d_run_02) %>%
+  mutate(
+    run = factor(run),
+    subid = factor(subid),
+    character = factor(character),
+    gender = factor(gender),
+    ethnicity = factor(ethnicity),
+    bgColor = factor(bgColor),
+    hoverTime = as.numeric(hoverTime),
+    rt = as.numeric(rt),
+    response = factor(response))
+
+glimpse(d_tidy)
+
+# --- WRITING ANONYMIZED CSV --------------------------------------------------
+
+# write to de-identified csv file
+write.csv(d_tidy, "/Users/kweisman/Documents/Research (Stanford)/Projects/Dimkid/dimkid/data/children/run-01_2016-07-14_anonymized.csv")
+
+d <- read.csv("/Users/kweisman/Documents/Research (Stanford)/Projects/Dimkid/dimkid/data/children/run-01_2016-07-14_anonymized.csv")          
+          
--- analysis/children/dimkid_prelim.R
+++ analysis/children/dimkid_prelim.R
@@ -22,22 +22,25 @@ library(tibble)
 
 d <- read.csv("/Users/kweisman/Documents/Research (Stanford)/Projects/Dimkid/dimkid/data/children/run-01_2016-07-14_anonymized.csv")
 
-d1 <- d %>%
+qplot(d$rt, bins = 100) +
+  scale_x_log10(breaks = seq(0, 1000, 100)) +
+  geom_vline(xintercept = 250, color = "red")
+
+d0 <- d %>%
+  filter(rt >= 250)
+
+d1 <- d0 %>%
   select(capacity, responseNum, subid) %>%
   filter(capacity != "na") %>%
   spread(capacity, responseNum)
-
 
 # continue
 
 d2 <- data.frame(d1[,-1], row.names = d1[,1])
 
-# d3 <- data.frame(d2[, !apply(is.na(d2), 2, any)])
-
 cor3 <- cor(d2, method = "spearman", use = "complete.obs")
 cor4 <- cor3
 cor4[is.na(cor4)] <- 1
-
 
 m <- as.matrix(d2)
 heatmap(m)
@@ -53,65 +56,4 @@ fa(cor4, nfactors = 3, rotate = "varimax")
 fa.sort(fa(cor4, nfactors = 4, rotate = "varimax")$loadings)
 fa.sort(fa(cor4, nfactors = 3, rotate = "varimax")$loadings)
 fa.sort(fa(cor4, nfactors = 2, rotate = "varimax")$loadings)
-
-
-
-
-# adults
-d <- read.csv("/Users/kweisman/Documents/Research (Stanford)/Projects/Dimkid/dimkid/data/adults/us_run-01_2016-06-05_anonymized.csv")          
-
-# plot log(rts)
-qplot(d$rt, bins = 100) + 
-  scale_x_log10(breaks = seq(0, 1000, 100)) +
-  geom_vline(xintercept = 250, color = "red")
-
-d0 <- d %>%
-  filter(rt >= 250)
-
-d1 <- d0 %>%
-  select(capWording, responseNum, subid) %>%
-  spread(capWording, responseNum)
-
-d2 <- data.frame(d1[,-1], row.names = d1[,1])
-
-# d3 <- data.frame(d2[, !apply(is.na(d2), 2, any)])
-
-cor3 <- cor(d2, method = "spearman", use = "complete.obs")
-cor4 <- cor3
-cor4[is.na(cor4)] <- 1
-
-m <- as.matrix(d2)
-heatmap(m)
-
-cluster <- hclust(dist(t(m)))
-plot(cluster)
-
-
-VSS.scree(cor4)
-fa(cor4, nfactors = 13, rotate = "none")
-# fa(cor4, nfactors = 4, rotate = "none")
-# fa(cor4, nfactors = 3, rotate = "none")
-fa(cor4, nfactors = 13, rotate = "varimax")
-# fa(cor4, nfactors = 4, rotate = "varimax")
-# fa(cor4, nfactors = 3, rotate = "varimax")
-fa.sort(fa(cor4, nfactors = 4, rotate = "varimax")$loadings[]) %>% View()
-fa.sort(fa(cor4, nfactors = 3, rotate = "varimax")$loadings[]) %>% View()
-# fa.sort(fa(cor4, nfactors = 2, rotate = "varimax")$loadings[]) %>% View()
-
-fa.sort(fa(cor4, nfactors = 3, rotate = "varimax")$loadings[]) %>% round(2) %>% data.frame() %>% rownames_to_column() %>% View()
-
-fa.sort(fa(cor4, nfactors = 4, rotate = "varimax")$loadings[]) %>% round(2) %>% data.frame() %>% rownames_to_column() %>% View()
-
-fa.sort(fa(cor4, nfactors = 7, rotate = "varimax")$loadings[]) %>% round(2) %>% data.frame() %>% rownames_to_column() %>% View()
-
-
-# loadings <- fa(cor4, nfactors = 3, rotate = "varimax")$loadings[] %>% data.frame() %>% rownames_to_column() %>% mutate(rowname = factor(rowname)) %>% arrange(rowname)
-# capacities <- data.frame(rowname = levels(d$capWording)) %>% arrange(rowname)
-# 
-# full_join(loadings, capacities) %>% View() 
-
-
-
-
-plot(fa(cor4, nfactors = 4, rotate = "varimax"))
 
