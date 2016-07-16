35aa91d90f2f78e57ae877576c67db456ac6ff36
kgweisman <kweisman@stanford.edu>
2016-07-15 19:38:17 -0700

    change to using dataframe instead of correlation matrix

--- analysis/adults/dimkid-adults_prelim.R
+++ analysis/adults/dimkid-adults_prelim.R
@@ -17,44 +17,40 @@ d0 <- d %>%
   filter(rt >= 250)
 
 d1 <- d0 %>%
-  select(capWording, responseNum, subid) %>%
-  spread(capWording, responseNum)
+  select(capacity, responseNum, subid) %>%
+  spread(capacity, responseNum)
 
 d2 <- data.frame(d1[,-1], row.names = d1[,1])
 
 cor3 <- cor(d2, method = "spearman", use = "complete.obs")
 
-m <- as.matrix(d2)
-heatmap(m)
-
-cluster <- hclust(dist(t(m)))
-plot(cluster)
-
-
-VSS.scree(cor3)
-fa(cor3, nfactors = 13, rotate = "none")
-# fa(cor3, nfactors = 4, rotate = "none")
-# fa(cor3, nfactors = 3, rotate = "none")
-fa(cor3, nfactors = 13, rotate = "varimax")
-# fa(cor3, nfactors = 4, rotate = "varimax")
-# fa(cor3, nfactors = 3, rotate = "varimax")
-fa.sort(fa(cor3, nfactors = 4, rotate = "varimax")$loadings[]) %>% View()
-fa.sort(fa(cor3, nfactors = 3, rotate = "varimax")$loadings[]) %>% View()
-# fa.sort(fa(cor3, nfactors = 2, rotate = "varimax")$loadings[]) %>% View()
-
-fa.sort(fa(cor3, nfactors = 3, rotate = "varimax")$loadings[]) %>% round(2) %>% data.frame() %>% rownames_to_column() %>% View()
-
-fa.sort(fa(cor3, nfactors = 4, rotate = "varimax")$loadings[]) %>% round(2) %>% data.frame() %>% rownames_to_column() %>% View()
-
-fa.sort(fa(cor3, nfactors = 7, rotate = "varimax")$loadings[]) %>% round(2) %>% data.frame() %>% rownames_to_column() %>% View()
+# m <- as.matrix(d2)
+# heatmap(m)
+# 
+# cluster <- hclust(dist(t(m)))
+# plot(cluster)
 
 
-# loadings <- fa(cor3, nfactors = 3, rotate = "varimax")$loadings[] %>% data.frame() %>% rownames_to_column() %>% mutate(rowname = factor(rowname)) %>% arrange(rowname)
+VSS.scree(d2)
+fa(r = d2, nfactors = 13, rotate = "none", fm = "minres", cor = "cor")
+fa(r = d2, nfactors = 13, rotate = "varimax", fm = "minres", cor = "cor")
+fa.sort(fa(d2, nfactors = 7, rotate = "varimax")$loadings[]) %>% View()
+fa.sort(fa(d2, nfactors = 4, rotate = "varimax")$loadings[]) %>% View()
+fa.sort(fa(d2, nfactors = 3, rotate = "varimax")$loadings[]) %>% View()
+
+# fa.sort(fa(d2, nfactors = 3, rotate = "varimax")$loadings[]) %>% round(2) %>% data.frame() %>% rownames_to_column() %>% View()
+# 
+# fa.sort(fa(d2, nfactors = 4, rotate = "varimax")$loadings[]) %>% round(2) %>% data.frame() %>% rownames_to_column() %>% View()
+# 
+# fa.sort(fa(d2, nfactors = 7, rotate = "varimax")$loadings[]) %>% round(2) %>% data.frame() %>% rownames_to_column() %>% View()
+
+
+# loadings <- fa(d2, nfactors = 3, rotate = "varimax")$loadings[] %>% data.frame() %>% rownames_to_column() %>% mutate(rowname = factor(rowname)) %>% arrange(rowname)
 # capacities <- data.frame(rowname = levels(d$capWording)) %>% arrange(rowname)
 # 
 # full_join(loadings, capacities) %>% View() 
 
-plot(fa(cor3, nfactors = 4, rotate = "varimax"))
+plot(fa(d2, nfactors = 4, rotate = "varimax"))
 
 
 
@@ -64,55 +60,56 @@ plot(fa(cor3, nfactors = 4, rotate = "varimax"))
 # remove items with mandatory definitions
 d3a <- d2[!grepl("\\.\\.\\.", names(d2))]
 
-cor3a <- cor(d3a, method = "spearman", use = "complete.obs")
+d3a <- cor(d3a, method = "spearman", use = "complete.obs")
 
-VSS.scree(cor3a)
-fa(cor3a, nfactors = 13, rotate = "none")
-# fa(cor3a, nfactors = 4, rotate = "none")
-# fa(cor3a, nfactors = 3, rotate = "none")
-fa(cor3a, nfactors = 13, rotate = "varimax")
-# fa(cor3a, nfactors = 4, rotate = "varimax")
-# fa(cor3a, nfactors = 3, rotate = "varimax")
-fa.sort(fa(cor3a, nfactors = 5, rotate = "varimax")$loadings[]) %>% View()
-fa.sort(fa(cor3a, nfactors = 4, rotate = "varimax")$loadings[]) %>% View()
-fa.sort(fa(cor3a, nfactors = 3, rotate = "varimax")$loadings[]) %>% View()
-# fa.sort(fa(cor3a, nfactors = 2, rotate = "varimax")$loadings[]) %>% View()
+VSS.scree(d3a)
+fa(d3a, nfactors = 13, rotate = "none")
+fa(d3a, nfactors = 13, rotate = "none", n.obs = 200)
+# fa(d3a, nfactors = 4, rotate = "none")
+# fa(d3a, nfactors = 3, rotate = "none")
+fa(d3a, nfactors = 13, rotate = "varimax")
+# fa(d3a, nfactors = 4, rotate = "varimax")
+# fa(d3a, nfactors = 3, rotate = "varimax")
+fa.sort(fa(d3a, nfactors = 5, rotate = "varimax")$loadings[]) %>% View()
+fa.sort(fa(d3a, nfactors = 4, rotate = "varimax")$loadings[]) %>% View()
+fa.sort(fa(d3a, nfactors = 3, rotate = "varimax")$loadings[]) %>% View()
+# fa.sort(fa(d3a, nfactors = 2, rotate = "varimax")$loadings[]) %>% View()
 
 
 # remove "cognitive" agency items
 d3b <- d2 %>%
   select (-decide.what.to.do, -figure.out.how.to.do.things, -have.self.control....like.when.you.stop.yourself.from.doing.something.you.shouldn.t.do)
 
-cor3b <- cor(d3b, method = "spearman", use = "complete.obs")
+d3b <- cor(d3b, method = "spearman", use = "complete.obs")
 
-VSS.scree(cor3b)
-fa(cor3b, nfactors = 13, rotate = "none")
-# fa(cor3b, nfactors = 4, rotate = "none")
-# fa(cor3b, nfactors = 3, rotate = "none")
-fa(cor3b, nfactors = 13, rotate = "varimax")
-# fa(cor3b, nfactors = 4, rotate = "varimax")
-# fa(cor3b, nfactors = 3, rotate = "varimax")
-# fa.sort(fa(cor3b, nfactors = 5, rotate = "varimax")$loadings[]) %>% View()
-# fa.sort(fa(cor3b, nfactors = 4, rotate = "varimax")$loadings[]) %>% View()
-fa.sort(fa(cor3b, nfactors = 3, rotate = "varimax")$loadings[]) %>% View()
-# fa.sort(fa(cor3b, nfactors = 2, rotate = "varimax")$loadings[]) %>% View()
+VSS.scree(d3b)
+fa(d3b, nfactors = 13, rotate = "none")
+# fa(d3b, nfactors = 4, rotate = "none")
+# fa(d3b, nfactors = 3, rotate = "none")
+fa(d3b, nfactors = 13, rotate = "varimax")
+# fa(d3b, nfactors = 4, rotate = "varimax")
+# fa(d3b, nfactors = 3, rotate = "varimax")
+# fa.sort(fa(d3b, nfactors = 5, rotate = "varimax")$loadings[]) %>% View()
+# fa.sort(fa(d3b, nfactors = 4, rotate = "varimax")$loadings[]) %>% View()
+fa.sort(fa(d3b, nfactors = 3, rotate = "varimax")$loadings[]) %>% View()
+# fa.sort(fa(d3b, nfactors = 2, rotate = "varimax")$loadings[]) %>% View()
 
 # remove (a priori) "substnantially reworded" agency items (see Mental capacity items 2016-06-30.doc)
 d3c <- d2 %>%
   select (-do.math, -be.aware.of.things, -feel.sad, -sense.whether.something.is.close.by.or.far.away, -get.hurt.feelings, -feel.scared, -decide.what.to.do, -make.plans, -know.what.s.nice.and.what.s.mean, -feel.sick....like.when.you.feel.like.you.might.throw.up, -smell.things, -figure.out.how.to.do.things, -be.aware.of.itself, -have.self.control....like.when.you.stop.yourself.from.doing.something.you.shouldn.t.do, -hear.sounds)
 
-cor3c <- cor(d3c, method = "spearman", use = "complete.obs")
+d3c <- cor(d3c, method = "spearman", use = "complete.obs")
 
-VSS.scree(cor3c)
-fa(cor3c, nfactors = 13, rotate = "none")
-# fa(cor3c, nfactors = 4, rotate = "none")
-# fa(cor3c, nfactors = 3, rotate = "none")
-fa(cor3c, nfactors = 13, rotate = "varimax")
-# fa(cor3c, nfactors = 4, rotate = "varimax")
-# fa(cor3c, nfactors = 3, rotate = "varimax")
-# fa.sort(fa(cor3c, nfactors = 5, rotate = "varimax")$loadings[]) %>% View()
-# fa.sort(fa(cor3c, nfactors = 4, rotate = "varimax")$loadings[]) %>% View()
-fa.sort(fa(cor3c, nfactors = 3, rotate = "varimax")$loadings[]) %>% View()
-# fa.sort(fa(cor3c, nfactors = 2, rotate = "varimax")$loadings[]) %>% View()
+VSS.scree(d3c)
+fa(d3c, nfactors = 13, rotate = "none")
+# fa(d3c, nfactors = 4, rotate = "none")
+# fa(d3c, nfactors = 3, rotate = "none")
+fa(d3c, nfactors = 13, rotate = "varimax")
+# fa(d3c, nfactors = 4, rotate = "varimax")
+# fa(d3c, nfactors = 3, rotate = "varimax")
+# fa.sort(fa(d3c, nfactors = 5, rotate = "varimax")$loadings[]) %>% View()
+# fa.sort(fa(d3c, nfactors = 4, rotate = "varimax")$loadings[]) %>% View()
+fa.sort(fa(d3c, nfactors = 3, rotate = "varimax")$loadings[]) %>% View()
+# fa.sort(fa(d3c, nfactors = 2, rotate = "varimax")$loadings[]) %>% View()
 
 
