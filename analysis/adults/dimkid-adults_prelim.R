4abcc3c6f25f82341e6df289928735a57d6ca45e
kgweisman <kweisman@stanford.edu>
2016-07-17 14:11:21 -0700

    comment out exploratory

--- analysis/adults/dimkid-adults_prelim.R
+++ analysis/adults/dimkid-adults_prelim.R
@@ -55,61 +55,61 @@ plot(fa(d2, nfactors = 4, rotate = "varimax"))
 
 
 
-#### EXPLORATORY
-
-# remove items with mandatory definitions
-d3a <- d2[!grepl("\\.\\.\\.", names(d2))]
-
-cor3a <- cor(d3a, method = "spearman", use = "complete.obs")
-
-VSS.scree(d3a)
-fa(d3a, nfactors = 13, rotate = "none")
-fa(d3a, nfactors = 13, rotate = "none", n.obs = 200)
-# fa(d3a, nfactors = 4, rotate = "none")
-# fa(d3a, nfactors = 3, rotate = "none")
-fa(d3a, nfactors = 13, rotate = "varimax")
-# fa(d3a, nfactors = 4, rotate = "varimax")
-# fa(d3a, nfactors = 3, rotate = "varimax")
-fa.sort(fa(d3a, nfactors = 5, rotate = "varimax")$loadings[]) %>% View()
-fa.sort(fa(d3a, nfactors = 4, rotate = "varimax")$loadings[]) %>% View()
-fa.sort(fa(d3a, nfactors = 3, rotate = "varimax")$loadings[]) %>% View()
-# fa.sort(fa(d3a, nfactors = 2, rotate = "varimax")$loadings[]) %>% View()
-
-
-# remove "cognitive" agency items
-d3b <- d2 %>%
-  select (-decide.what.to.do, -figure.out.how.to.do.things, -have.self.control....like.when.you.stop.yourself.from.doing.something.you.shouldn.t.do)
-
-cor3b <- cor(d3b, method = "spearman", use = "complete.obs")
-
-VSS.scree(d3b)
-fa(d3b, nfactors = 13, rotate = "none")
-# fa(d3b, nfactors = 4, rotate = "none")
-# fa(d3b, nfactors = 3, rotate = "none")
-fa(d3b, nfactors = 13, rotate = "varimax")
-# fa(d3b, nfactors = 4, rotate = "varimax")
-# fa(d3b, nfactors = 3, rotate = "varimax")
-# fa.sort(fa(d3b, nfactors = 5, rotate = "varimax")$loadings[]) %>% View()
-# fa.sort(fa(d3b, nfactors = 4, rotate = "varimax")$loadings[]) %>% View()
-fa.sort(fa(d3b, nfactors = 3, rotate = "varimax")$loadings[]) %>% View()
-# fa.sort(fa(d3b, nfactors = 2, rotate = "varimax")$loadings[]) %>% View()
-
-# remove (a priori) "substnantially reworded" agency items (see Mental capacity items 2016-06-30.doc)
-d3c <- d2 %>%
-  select (-do.math, -be.aware.of.things, -feel.sad, -sense.whether.something.is.close.by.or.far.away, -get.hurt.feelings, -feel.scared, -decide.what.to.do, -make.plans, -know.what.s.nice.and.what.s.mean, -feel.sick....like.when.you.feel.like.you.might.throw.up, -smell.things, -figure.out.how.to.do.things, -be.aware.of.itself, -have.self.control....like.when.you.stop.yourself.from.doing.something.you.shouldn.t.do, -hear.sounds)
-
-cor3c <- cor(d3c, method = "spearman", use = "complete.obs")
-
-VSS.scree(d3c)
-fa(d3c, nfactors = 13, rotate = "none")
-# fa(d3c, nfactors = 4, rotate = "none")
-# fa(d3c, nfactors = 3, rotate = "none")
-fa(d3c, nfactors = 13, rotate = "varimax")
-# fa(d3c, nfactors = 4, rotate = "varimax")
-# fa(d3c, nfactors = 3, rotate = "varimax")
-# fa.sort(fa(d3c, nfactors = 5, rotate = "varimax")$loadings[]) %>% View()
-# fa.sort(fa(d3c, nfactors = 4, rotate = "varimax")$loadings[]) %>% View()
-fa.sort(fa(d3c, nfactors = 3, rotate = "varimax")$loadings[]) %>% View()
-# fa.sort(fa(d3c, nfactors = 2, rotate = "varimax")$loadings[]) %>% View()
-
-
+# #### EXPLORATORY
+# 
+# # remove items with mandatory definitions
+# d3a <- d2[!grepl("\\.\\.\\.", names(d2))]
+# 
+# cor3a <- cor(d3a, method = "spearman", use = "complete.obs")
+# 
+# VSS.scree(d3a)
+# fa(d3a, nfactors = 13, rotate = "none")
+# fa(d3a, nfactors = 13, rotate = "none", n.obs = 200)
+# # fa(d3a, nfactors = 4, rotate = "none")
+# # fa(d3a, nfactors = 3, rotate = "none")
+# fa(d3a, nfactors = 13, rotate = "varimax")
+# # fa(d3a, nfactors = 4, rotate = "varimax")
+# # fa(d3a, nfactors = 3, rotate = "varimax")
+# fa.sort(fa(d3a, nfactors = 5, rotate = "varimax")$loadings[]) %>% View()
+# fa.sort(fa(d3a, nfactors = 4, rotate = "varimax")$loadings[]) %>% View()
+# fa.sort(fa(d3a, nfactors = 3, rotate = "varimax")$loadings[]) %>% View()
+# # fa.sort(fa(d3a, nfactors = 2, rotate = "varimax")$loadings[]) %>% View()
+# 
+# 
+# # remove "cognitive" agency items
+# d3b <- d2 %>%
+#   select (-decide.what.to.do, -figure.out.how.to.do.things, -have.self.control....like.when.you.stop.yourself.from.doing.something.you.shouldn.t.do)
+# 
+# cor3b <- cor(d3b, method = "spearman", use = "complete.obs")
+# 
+# VSS.scree(d3b)
+# fa(d3b, nfactors = 13, rotate = "none")
+# # fa(d3b, nfactors = 4, rotate = "none")
+# # fa(d3b, nfactors = 3, rotate = "none")
+# fa(d3b, nfactors = 13, rotate = "varimax")
+# # fa(d3b, nfactors = 4, rotate = "varimax")
+# # fa(d3b, nfactors = 3, rotate = "varimax")
+# # fa.sort(fa(d3b, nfactors = 5, rotate = "varimax")$loadings[]) %>% View()
+# # fa.sort(fa(d3b, nfactors = 4, rotate = "varimax")$loadings[]) %>% View()
+# fa.sort(fa(d3b, nfactors = 3, rotate = "varimax")$loadings[]) %>% View()
+# # fa.sort(fa(d3b, nfactors = 2, rotate = "varimax")$loadings[]) %>% View()
+# 
+# # remove (a priori) "substnantially reworded" agency items (see Mental capacity items 2016-06-30.doc)
+# d3c <- d2 %>%
+#   select (-do.math, -be.aware.of.things, -feel.sad, -sense.whether.something.is.close.by.or.far.away, -get.hurt.feelings, -feel.scared, -decide.what.to.do, -make.plans, -know.what.s.nice.and.what.s.mean, -feel.sick....like.when.you.feel.like.you.might.throw.up, -smell.things, -figure.out.how.to.do.things, -be.aware.of.itself, -have.self.control....like.when.you.stop.yourself.from.doing.something.you.shouldn.t.do, -hear.sounds)
+# 
+# cor3c <- cor(d3c, method = "spearman", use = "complete.obs")
+# 
+# VSS.scree(d3c)
+# fa(d3c, nfactors = 13, rotate = "none")
+# # fa(d3c, nfactors = 4, rotate = "none")
+# # fa(d3c, nfactors = 3, rotate = "none")
+# fa(d3c, nfactors = 13, rotate = "varimax")
+# # fa(d3c, nfactors = 4, rotate = "varimax")
+# # fa(d3c, nfactors = 3, rotate = "varimax")
+# # fa.sort(fa(d3c, nfactors = 5, rotate = "varimax")$loadings[]) %>% View()
+# # fa.sort(fa(d3c, nfactors = 4, rotate = "varimax")$loadings[]) %>% View()
+# fa.sort(fa(d3c, nfactors = 3, rotate = "varimax")$loadings[]) %>% View()
+# # fa.sort(fa(d3c, nfactors = 2, rotate = "varimax")$loadings[]) %>% View()
+# 
+# 
