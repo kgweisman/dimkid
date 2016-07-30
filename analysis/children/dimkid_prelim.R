cf05553b07b766e50841f3e844f1bcff6ca3889c
kgweisman <kweisman@stanford.edu>
2016-07-30 10:40:28 -0700

    add new data; add plots of average scores, separate by conditions

--- analysis/children/dimkid_prelim.R
+++ analysis/children/dimkid_prelim.R
@@ -19,7 +19,7 @@ d1_pilot <- d_pilot %>%
 
 # lydia, olivia, allie run
 
-d <- read.csv("/Users/kweisman/Documents/Research (Stanford)/Projects/Dimkid/dimkid/data/children/run-01_2016-07-27_anonymized.csv")
+d <- read.csv("/Users/kweisman/Documents/Research (Stanford)/Projects/Dimkid/dimkid/data/children/run-01_2016-07-29_anonymized.csv")
 
 qplot(d$rt, bins = 100) +
   scale_x_log10(breaks = seq(0, 1000, 100)) +
@@ -91,10 +91,77 @@ fa.sort(fa(d2, nfactors = 4, rotate = "varimax")$loadings[]) %>% View()
 fa.sort(fa(d2, nfactors = 3, rotate = "varimax")$loadings[]) %>% View()
 
 fa.parallel(d2, cor = "poly")
-fa.sort(fa(d2, nfactors = 5, rotate = "varimax", cor = "poly")$loadings[]) %>% View()
+fa.sort(fa(d2, nfactors = 6, rotate = "varimax", cor = "poly")$loadings[]) %>% View()
 fa.sort(fa(d2, nfactors = 3, rotate = "varimax", cor = "poly")$loadings[]) %>% View()
 
+# make factor assignments
+factors <- fa.sort(fa(d2, nfactors = 3, rotate = "varimax", cor = "poly")$loadings[]) 
+
+factors2 <- factors %>%
+  data.frame() %>%
+  rownames_to_column(var = "capacity") %>%
+  mutate(MR1_abs = abs(MR1),
+         MR2_abs = abs(MR2),
+         MR3_abs = abs(MR3),
+         loading_abs = pmax(MR1_abs, MR2_abs, MR3_abs),
+         loading = ifelse(loading_abs == abs(MR1), MR1,
+                          ifelse(loading_abs == abs(MR2), MR2,
+                                 ifelse(loading_abs == abs(MR3), MR3,
+                                        NA))),
+         factor = ifelse(loading == MR1, "MR1",
+                         ifelse(loading == MR2, "MR2",
+                                ifelse(loading == MR3, "MR3",
+                                       NA)))) %>%
+  arrange(factor, desc(loading_abs)) %>%
+  select(capacity, factor, loading, loading_abs)
+
+factors3 <- 
+  full_join(factors2 %>%
+              filter(factor == "MR1") %>%
+              rownames_to_column(var = "order") %>%
+              mutate(order = as.numeric(order)),
+            factors2 %>%
+              filter(factor == "MR2") %>%
+              rownames_to_column(var = "order") %>%
+              mutate(order = as.numeric(order))) %>%
+  full_join(factors2 %>%
+              filter(factor == "MR3") %>%
+              rownames_to_column(var = "order") %>%
+              mutate(order = as.numeric(order))) %>%
+  mutate(posneg = factor(ifelse(loading < 0, "neg", "pos")))
+
 # by condition
+d1_bycond <- d0 %>%
+  select(character, capacity, responseNum, subid) %>%
+  filter(capacity != "na", is.na(responseNum) == F)
+
+library(langcog)
+d1_bycond_mb <- multi_boot(d1_bycond,
+                           column = "responseNum",
+                           summary_groups = c("character", "capacity"),
+                           statistics_functions = c("mean", "ci_lower", "ci_upper")) %>% 
+  full_join(factors3) %>%
+  arrange(character, factor, desc(loading_abs)) %>%
+  rownames_to_column(var = "full_order") %>%
+  mutate(full_order = as.numeric(full_order))
+
+ggplot(d1_bycond_mb, 
+       aes(x = desc(order), y = mean,
+           group = character, color = character,
+           label = paste0(capacity, ": ", 
+                          formatC(round(loading, 2), 
+                                  format = "f",
+                                  digits = 2)))) +
+  facet_grid(. ~ factor) +
+  geom_point(stat = "identity", position = position_dodge(width = 1)) +
+  geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper),
+                position = position_dodge(width = 1), width = 0) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme_bw() +
+  geom_text(aes(y = -0.18, hjust = 0, color = posneg)) +
+  coord_flip()
+
+# separate by character
 d1_robot <- d0 %>%
   filter(character == "robot") %>%
   select(capacity, responseNum, subid) %>%
