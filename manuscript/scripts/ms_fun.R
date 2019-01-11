8f071a85cd7c399401120f84799fb2c2c442315b
kgweisman <kweisman@stanford.edu>
2019-01-11 11:00:26 -0500

    move manuscript functions into source file

---
+++ manuscript/scripts/ms_fun.R
@@ -0,0 +1,34 @@
+# function for quickly getting counts and proportions for categorical demo variables
+demo_fun <- function(df, var, n_round = NA){
+  new_df <- df %>%
+    distinct_("subid", var) %>%
+    mutate_at(.vars = var,
+              .funs = . %>% as.character() %>% replace_na(., "MISSING")) %>%
+    count_(var) %>%
+    mutate(prop = n/sum(n)) %>%
+    arrange(desc(n))
+  if(!is.na(n_round)){
+    new_df <- new_df %>% mutate(prop = round(prop, n_round))
+  }
+  return(new_df)
+}
+
+# function for getting write-up of brms model results
+write_b_95CI_fun <- function(model, param, round_n = 2){
+  fixef <- fixef(model) %>% round(round_n) %>% format(nsmall = round_n)
+  b <- fixef[param, "Estimate"]
+  lower <- fixef[param, "Q2.5"]
+  upper <- fixef[param, "Q97.5"]
+  text <- paste0("_b_ = ", b, 
+                 ", 95% credible interval: [", lower, ", ", upper, "]")
+  text <- gsub("\\[ ", "\\[", text)
+  return(text)
+}
+
+binomial_smooth <- function(...) {
+  geom_smooth(method = "glm", method.args = list(family = "binomial"), ...)
+}
+
+ital_by_cap_fun <- function(str) {
+  paste0("_", paste(str, collapse = "_, _"), "_")
+}
