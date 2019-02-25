3645c7f271e7308bd37b534908587802c941b3c6
kgweisman <kweisman@stanford.edu>
2019-02-25 16:35:41 -0500

    table 3.1, factor retention

--- dissertation/scripts/ms_fun.R
+++ dissertation/scripts/ms_fun.R
@@ -29,6 +29,30 @@ demo_fun <- function(df, var, n_round = NA){
   return(new_df)
 }
 
+# function for running factor retention protocols across multiple datasets
+reten_report_fun <- function(df_list){
+  
+  res <- data.frame(par = NULL, bic = NULL, wdm = NULL)
+  
+  for(i in 1:length(df_list)){
+    nfact_par <- fa.parallel(df_list[[i]],
+                             cor = chosen_cor, fm = chosen_fm,
+                             plot = F)$nfact
+    
+    nfact_bic <- vss(df_list[[i]], 
+                     cor = chosen_cor, rotate = chosen_rot, fm = chosen_fm,
+                     plot = F)$vss.stats$BIC %>% which.min()
+    
+    nfact_wdm <- reten_fun(df_list[[i]], rot_type = chosen_rot)
+    
+    res[names(df_list)[i], "par"] <- nfact_par
+    res[names(df_list)[i], "bic"] <- nfact_bic
+    res[names(df_list)[i], "wdm"] <- nfact_wdm
+  }
+  
+  return(res)
+}
+
 # function for getting write-up of brms model results
 write_b_95CI_fun <- function(model, param, round_n = 2){
   fixef <- fixef(model) %>% round(round_n) %>% format(nsmall = round_n)
@@ -41,6 +65,7 @@ write_b_95CI_fun <- function(model, param, round_n = 2){
   return(text)
 }
 
+# functions for plotting
 binomial_smooth <- function(...) {
   geom_smooth(method = "glm", method.args = list(family = "binomial"), ...)
 }
