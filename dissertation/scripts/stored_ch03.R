502eecd3abc9c94b46ab27844c9823c03a560c52
kgweisman <kweisman@stanford.edu>
2019-03-10 15:10:06 -0400

    reorganization

--- dissertation/scripts/stored_ch03.R
+++ dissertation/scripts/stored_ch03.R
@@ -2,86 +2,86 @@
 
 # load in EFAs
 # study 1
-efa_par_d1a_ad <- readRDS(file = "./efas/efa_par_d1a_ad")
-efa_bic_d1a_ad <- readRDS(file = "./efas/efa_bic_d1a_ad")
-efa_wdm_d1a_ad <- readRDS(file = "./efas/efa_wdm_d1a_ad")
+efa_par_d1a_ad <- readRDS(file = "./stored/efas/efa_par_d1a_ad")
+efa_bic_d1a_ad <- readRDS(file = "./stored/efas/efa_bic_d1a_ad")
+efa_wdm_d1a_ad <- readRDS(file = "./stored/efas/efa_wdm_d1a_ad")
 
-efa_par_d1b_ad <- readRDS(file = "./efas/efa_par_d1b_ad")
-efa_bic_d1b_ad <- readRDS(file = "./efas/efa_bic_d1b_ad")
-efa_wdm_d1b_ad <- readRDS(file = "./efas/efa_wdm_d1b_ad")
+efa_par_d1b_ad <- readRDS(file = "./stored/efas/efa_par_d1b_ad")
+efa_bic_d1b_ad <- readRDS(file = "./stored/efas/efa_bic_d1b_ad")
+efa_wdm_d1b_ad <- readRDS(file = "./stored/efas/efa_wdm_d1b_ad")
 
-efa_par_d1c_ad <- readRDS(file = "./efas/efa_par_d1c_ad")
-efa_bic_d1c_ad <- readRDS(file = "./efas/efa_bic_d1c_ad")
-efa_wdm_d1c_ad <- readRDS(file = "./efas/efa_wdm_d1c_ad")
+efa_par_d1c_ad <- readRDS(file = "./stored/efas/efa_par_d1c_ad")
+efa_bic_d1c_ad <- readRDS(file = "./stored/efas/efa_bic_d1c_ad")
+efa_wdm_d1c_ad <- readRDS(file = "./stored/efas/efa_wdm_d1c_ad")
 
-efa_par_d1d_ad <- readRDS(file = "./efas/efa_par_d1d_ad")
-efa_bic_d1d_ad <- readRDS(file = "./efas/efa_bic_d1d_ad")
-efa_wdm_d1d_ad <- readRDS(file = "./efas/efa_wdm_d1d_ad")
+efa_par_d1d_ad <- readRDS(file = "./stored/efas/efa_par_d1d_ad")
+efa_bic_d1d_ad <- readRDS(file = "./stored/efas/efa_bic_d1d_ad")
+efa_wdm_d1d_ad <- readRDS(file = "./stored/efas/efa_wdm_d1d_ad")
 
 # study 2
-efa_par_d2_ad <- readRDS(file = "./efas/efa_par_d2_ad")
-efa_bic_d2_ad <- readRDS(file = "./efas/efa_bic_d2_ad")
-efa_wdm_d2_ad <- readRDS(file = "./efas/efa_wdm_d2_ad")
+efa_par_d2_ad <- readRDS(file = "./stored/efas/efa_par_d2_ad")
+efa_bic_d2_ad <- readRDS(file = "./stored/efas/efa_bic_d2_ad")
+efa_wdm_d2_ad <- readRDS(file = "./stored/efas/efa_wdm_d2_ad")
 
-efa_par_d2_79 <- readRDS(file = "./efas/efa_par_d2_79")
-efa_bic_d2_79 <- readRDS(file = "./efas/efa_bic_d2_79")
-efa_wdm_d2_79 <- readRDS(file = "./efas/efa_wdm_d2_79")
+efa_par_d2_79 <- readRDS(file = "./stored/efas/efa_par_d2_79")
+efa_bic_d2_79 <- readRDS(file = "./stored/efas/efa_bic_d2_79")
+efa_wdm_d2_79 <- readRDS(file = "./stored/efas/efa_wdm_d2_79")
 
 # study 3
-efa_par_d3_ad <- readRDS(file = "./efas/efa_par_d3_ad")
-efa_bic_d3_ad <- readRDS(file = "./efas/efa_bic_d3_ad")
-efa_wdm_d3_ad <- readRDS(file = "./efas/efa_wdm_d3_ad")
+efa_par_d3_ad <- readRDS(file = "./stored/efas/efa_par_d3_ad")
+efa_bic_d3_ad <- readRDS(file = "./stored/efas/efa_bic_d3_ad")
+efa_wdm_d3_ad <- readRDS(file = "./stored/efas/efa_wdm_d3_ad")
 
-efa_par_d3_79 <- readRDS(file = "./efas/efa_par_d3_79")
-efa_bic_d3_79 <- readRDS(file = "./efas/efa_bic_d3_79")
-efa_wdm_d3_79 <- readRDS(file = "./efas/efa_wdm_d3_79")
+efa_par_d3_79 <- readRDS(file = "./stored/efas/efa_par_d3_79")
+efa_bic_d3_79 <- readRDS(file = "./stored/efas/efa_bic_d3_79")
+efa_wdm_d3_79 <- readRDS(file = "./stored/efas/efa_wdm_d3_79")
 
-efa_par_d3_46 <- readRDS(file = "./efas/efa_par_d3_46")
-efa_bic_d3_46 <- readRDS(file = "./efas/efa_bic_d3_46")
-efa_wdm_d3_46 <- readRDS(file = "./efas/efa_wdm_d3_46")
+efa_par_d3_46 <- readRDS(file = "./stored/efas/efa_par_d3_46")
+efa_bic_d3_46 <- readRDS(file = "./stored/efas/efa_bic_d3_46")
+efa_wdm_d3_46 <- readRDS(file = "./stored/efas/efa_wdm_d3_46")
 
 # study 4
-efa_par_d4_ad <- readRDS(file = "./efas/efa_par_d4_ad")
-efa_bic_d4_ad <- readRDS(file = "./efas/efa_bic_d4_ad")
-efa_wdm_d4_ad <- readRDS(file = "./efas/efa_wdm_d4_ad")
+efa_par_d4_ad <- readRDS(file = "./stored/efas/efa_par_d4_ad")
+efa_bic_d4_ad <- readRDS(file = "./stored/efas/efa_bic_d4_ad")
+efa_wdm_d4_ad <- readRDS(file = "./stored/efas/efa_wdm_d4_ad")
 
-efa_par_d4_46 <- readRDS(file = "./efas/efa_par_d4_46")
-efa_bic_d4_46 <- readRDS(file = "./efas/efa_bic_d4_46")
-efa_wdm_d4_46 <- readRDS(file = "./efas/efa_wdm_d4_46")
+efa_par_d4_46 <- readRDS(file = "./stored/efas/efa_par_d4_46")
+efa_bic_d4_46 <- readRDS(file = "./stored/efas/efa_bic_d4_46")
+efa_wdm_d4_46 <- readRDS(file = "./stored/efas/efa_wdm_d4_46")
 
 # load in factor names
 # study 1
 factor_names_efa_wdm_d1a_ad <- readRDS( 
-  file = "./factor_names/factor_names_efa_wdm_d1a_ad")
+  file = "./stored/factor_names/factor_names_efa_wdm_d1a_ad")
 factor_names_efa_wdm_d1b_ad <- readRDS( 
-  file = "./factor_names/factor_names_efa_wdm_d1b_ad")
+  file = "./stored/factor_names/factor_names_efa_wdm_d1b_ad")
 factor_names_efa_wdm_d1c_ad <- readRDS( 
-  file = "./factor_names/factor_names_efa_wdm_d1c_ad")
+  file = "./stored/factor_names/factor_names_efa_wdm_d1c_ad")
 factor_names_efa_wdm_d1d_ad <- readRDS( 
-  file = "./factor_names/factor_names_efa_wdm_d1d_ad")
+  file = "./stored/factor_names/factor_names_efa_wdm_d1d_ad")
 
 # study 2
 factor_names_efa_wdm_d2_ad <- readRDS( 
-  file = "./factor_names/factor_names_efa_wdm_d2_ad")
+  file = "./stored/factor_names/factor_names_efa_wdm_d2_ad")
 factor_names_efa_par_d2_ad <- readRDS( 
-  file = "./factor_names/factor_names_efa_par_d2_ad")
+  file = "./stored/factor_names/factor_names_efa_par_d2_ad")
 factor_names_efa_wdm_d2_79 <- readRDS( 
-  file = "./factor_names/factor_names_efa_wdm_d2_79")
+  file = "./stored/factor_names/factor_names_efa_wdm_d2_79")
 
 # study 3
 factor_names_efa_wdm_d3_ad <- readRDS( 
-  file = "./factor_names/factor_names_efa_wdm_d3_ad")
+  file = "./stored/factor_names/factor_names_efa_wdm_d3_ad")
 factor_names_efa_wdm_d3_79 <- readRDS( 
-  file = "./factor_names/factor_names_efa_wdm_d3_79")
+  file = "./stored/factor_names/factor_names_efa_wdm_d3_79")
 factor_names_efa_wdm_d3_46 <- readRDS( 
-  file = "./factor_names/factor_names_efa_wdm_d3_46")
+  file = "./stored/factor_names/factor_names_efa_wdm_d3_46")
 factor_names_efa_par_d3_46 <- readRDS( 
-  file = "./factor_names/factor_names_efa_par_d3_46")
+  file = "./stored/factor_names/factor_names_efa_par_d3_46")
 
 # study 4
 factor_names_efa_wdm_d4_ad <- readRDS( 
-  file = "./factor_names/factor_names_efa_wdm_d4_ad")
+  file = "./stored/factor_names/factor_names_efa_wdm_d4_ad")
 factor_names_efa_par_d4_46 <- readRDS( 
-  file = "./factor_names/factor_names_efa_par_d4_46")
+  file = "./stored/factor_names/factor_names_efa_par_d4_46")
 factor_names_efa_wdm_d4_46 <- readRDS( 
-  file = "./factor_names/factor_names_efa_wdm_d4_46")
+  file = "./stored/factor_names/factor_names_efa_wdm_d4_46")
---
+++ dissertation/scripts/stored_ch04.R
@@ -0,0 +1,30 @@
+# STORED SCALES, SCORED DATA, DIFFSCORE DATA, ETC. (CHAPTER IV)
+
+# load in scales
+# study 1
+
+# study 2
+
+# study 3
+
+# study 4
+
+
+# load in scored data
+# study 1
+
+# study 2
+
+# study 3
+
+# study 4
+
+
+# load in diffscore data
+# study 1
+
+# study 2
+
+# study 3
+
+# study 4
