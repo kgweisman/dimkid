2448d13e0e5fc13f9ea400c37de91d2cc4623342
kgweisman <kweisman@stanford.edu>
2019-03-10 19:45:51 -0400

    move things from ch04 to appendix, redo ch04 tables

--- dissertation/scripts/stored_ch04.R
+++ dissertation/scripts/stored_ch04.R
@@ -2,12 +2,22 @@
 
 # load in scales
 # study 1
+scales_efa_wdm_d1a_ad <- readRDS("./stored/scales/scales_efa_wdm_d1a_ad")
+scales_efa_wdm_d1b_ad <- readRDS("./stored/scales/scales_efa_wdm_d1b_ad")
+scales_efa_wdm_d1c_ad <- readRDS("./stored/scales/scales_efa_wdm_d1c_ad")
+scales_efa_wdm_d1d_ad <- readRDS("./stored/scales/scales_efa_wdm_d1d_ad")
 
 # study 2
+scales_efa_wdm_d2_ad <- readRDS("./stored/scales/scales_efa_wdm_d2_ad")
+scales_efa_wdm_d2_79 <- readRDS("./stored/scales/scales_efa_wdm_d2_79")
 
 # study 3
+scales_efa_wdm_d3_ad <- readRDS("./stored/scales/scales_efa_wdm_d3_ad")
+scales_efa_wdm_d3_79 <- readRDS("./stored/scales/scales_efa_wdm_d3_79")
 
 # study 4
+scales_efa_wdm_d4_ad <- readRDS("./stored/scales/scales_efa_wdm_d4_ad")
+
 
 
 # load in scored data
