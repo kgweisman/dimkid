157f9448eca6e73e98c7ae47796f201d8489eb9d
kgweisman <kweisman@stanford.edu>
2019-03-15 16:52:57 -0400

    finish full draft of gd

--- dissertation/scripts/ms_fun.R
+++ dissertation/scripts/ms_fun.R
@@ -234,7 +234,7 @@ nonzero_fun <- function(regtab, which_pair,
 # function for getting % modal responding for diffscores
 modal_percent_fun <- function(table = diffscores_tab, which_pair, which_age_group){
   sum_tab <- table %>%
-    filter(pair == which_pair, age_group == which_age_group) %>%
+    filter(pair %in% which_pair, age_group == which_age_group) %>%
     summarise(min = min(modal), max = max(modal))
   
   min <- round(sum_tab$min, 2) * 100
