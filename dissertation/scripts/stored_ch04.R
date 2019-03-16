6ffefae1441f0698749c8d38431f185a6572bf69
kgweisman <kweisman@stanford.edu>
2019-03-16 17:51:45 -0400

    correct path errors

--- dissertation/scripts/stored_ch04.R
+++ dissertation/scripts/stored_ch04.R
@@ -22,23 +22,23 @@ scales_efa_wdm_d4_ad <- readRDS("./stored/scales/scales_efa_wdm_d4_ad")
 
 # load in scored data
 # study 1
-d1a_ad_scored_ad <- readRDS("./stored/diffscore_data/d1a_ad_scored_ad_diff")
-d1b_ad_scored_ad <- readRDS("./stored/diffscore_data/d1b_ad_scored_ad_diff")
-d1c_ad_scored_ad <- readRDS("./stored/diffscore_data/d1c_ad_scored_ad_diff")
-d1d_ad_scored_ad <- readRDS("./stored/diffscore_data/d1d_ad_scored_ad_diff")
+d1a_ad_scored_ad <- readRDS("./stored/scored_data/d1a_ad_scored_ad")
+d1b_ad_scored_ad <- readRDS("./stored/scored_data/d1b_ad_scored_ad")
+d1c_ad_scored_ad <- readRDS("./stored/scored_data/d1c_ad_scored_ad")
+d1d_ad_scored_ad <- readRDS("./stored/scored_data/d1d_ad_scored_ad")
 
 # study 2
-d2_ad_scored_ad <- readRDS("./stored/diffscore_data/d2_ad_scored_ad_diff")
-d2_79_scored_ad <- readRDS("./stored/diffscore_data/d2_79_scored_ad_diff")
+d2_ad_scored_ad <- readRDS("./stored/scored_data/d2_ad_scored_ad")
+d2_79_scored_ad <- readRDS("./stored/scored_data/d2_79_scored_ad")
 
 # study 3
-d3_ad_scored_ad <- readRDS("./stored/diffscore_data/d3_ad_scored_ad_diff")
-d3_79_scored_ad <- readRDS("./stored/diffscore_data/d3_79_scored_ad_diff")
-d3_46_scored_ad <- readRDS("./stored/diffscore_data/d3_46_scored_ad_diff")
+d3_ad_scored_ad <- readRDS("./stored/scored_data/d3_ad_scored_ad")
+d3_79_scored_ad <- readRDS("./stored/scored_data/d3_79_scored_ad")
+d3_46_scored_ad <- readRDS("./stored/scored_data/d3_46_scored_ad")
 
 # study 4
-d4_ad_scored_ad <- readRDS("./stored/diffscore_data/d4_ad_scored_ad_diff")
-d4_46_scored_ad <- readRDS("./stored/diffscore_data/d4_46_scored_ad_diff")
+d4_ad_scored_ad <- readRDS("./stored/scored_data/d4_ad_scored_ad")
+d4_46_scored_ad <- readRDS("./stored/scored_data/d4_46_scored_ad")
 
 
 # load in diffscore data
